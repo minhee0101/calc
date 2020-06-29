5c268a2e07cd20feab51c99700bf1040789faa7b
minhee0101 <alsgml2846@naver.com>
2020-06-29 18:01:45 +0900

    template.py add

---
+++ template.py
@@ -0,0 +1,16 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+             <input type="number" name="a"> , <input type="number" name="b">
+      
+        <input type="submit">
+        </form>
+    </body>
+
+    <p>
+        sum:%(x)s</br>
+        mul:%(y)s</br>
+    </p>
+</html>
+"""
